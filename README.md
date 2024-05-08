ea20077fa1907a4f13537d3cf3aa90dc88d49671
kavisek <kavi.skhon@gmail.com>
2024-05-07 20:16:06 -0700

    feat: adding psuilt dashboard

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-# Template Python App
+# Demo PSUTIL
 
-{description}
+This will render cpu usage and memory usage of the system.
+
+![Python](./app/images/images.png)
 
 ## Setup
 
 ```bash
 cd app
 poetry install
+
+
+# Packages
+poetry add psutil rich curses
+
+# Run app
+python3 main.py
 ```
 
 ## References
--- app/dashboard_cpu.py
+++ app/dashboard_cpu.py
@@ -1,29 +1,17 @@
 import curses
 import psutil
 
-def setup_colors():
-    # Initialize color pairs
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Green on black
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)    # Red on black
-
 def draw_bars(win, core_data):
     win.clear()
     h, w = win.getmaxyx()
     for i, usage in enumerate(core_data):
         bar_length = int((usage / 100.0) * (w - 20))
-        # Decide color based on usage
-        color = 1 if usage < 50 else 2
         bar = '█' * bar_length + '-' * (w - 20 - bar_length)
-        # Print each core's usage with appropriate color
-        win.addstr(i, 0, f"Core {i+1} — Efficiency: [", curses.color_pair(color))
-        win.addstr(bar, curses.color_pair(color))
-        win.addstr(f"] {usage:5.1f}%")
+        win.addstr(i, 0, f"Core {i+1} — Efficiency: [{bar}] {usage}%")
     win.refresh()
 
 def main(stdscr):
     curses.curs_set(0)  # Hide the cursor
-    setup_colors()
     stdscr.nodelay(True)  # Make getkey non-blocking
     while True:
         if stdscr.getch() == ord('q'):  # Press 'q' to quit
